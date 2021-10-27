6e9ed32a5df98b2277aecf19ac85167a23eb3445
André Nobre <afelipe1405@gmail.com>
2021-10-27 17:35:01 -0300

    Delete tkinter directory

--- tkinter/message.py
+++
@@ -1,8 +0,0 @@
-# Widget Message
-import tkinter as tk
-janela = tk.Tk()
-mensagem_para_usuario = 'Olá Usuário'
-msg = tk.Message(janela, text = mensagem_para_usuario)
-msg.config(bg = 'lightblue', font = ('verdana', 20, 'italic'))
-msg.pack()
-janela.mainloop()
